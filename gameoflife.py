a8b8c9650656d1654d71d52f6c8827d026f94f40
hilalipik <hilalipik@gmail.com>
2023-03-06 16:16:59 +0200

    updated according to code review

--- gameoflife.py
+++ gameoflife.py
@@ -1,8 +1,7 @@
 import time
 import copy
 import os
-import sys
-sys.argv
+import argparse
 
 ALIVE_FORMAT = '*'
 DEAD_FORMAT = ' '
@@ -19,51 +18,71 @@ MSG = '''
                                                               
                                                               '''
 
-def init_board(file_path):
-    b = []
+def init_board(file_path : str) ->list:
+    '''
+    Function initializes a board sccording to the content in file.
+    Input: file path.
+    Output: initialized board.
+    '''
+    board = []
     if not os.path.isfile(file_path):
         raise Exception(f"File {file_path} could not be found.")
     with open(file_path, mode="r") as file:
         file_content = file.readlines()
     for line in file_content:
-        b_line = []
+        board_line = []
         for cell in line:
             if cell == ALIVE_FORMAT:
-                b_line.append(ALIVE)
+                board_line.append(ALIVE)
             elif cell == DEAD_FORMAT:
-                b_line.append(DEAD)
+                board_line.append(DEAD)
             elif cell != ',' and cell !='\n':
                 raise Exception(f"File {file_path} is not written according format.")
-        b.append(b_line)
-    return b
+        board.append(board_line)
+    return board
 
 
-def sum_neighbors(row, col, board):
+def sum_neighbors(row : int, col : int, board : list) ->int:
+    '''
+    Function counts the alive neighbors around the wanted cell in the board.
+    Input: the wanted call location in board, board.
+    Output: the number of alive cells around the wanted cell.
+    '''
     
     neighbors_count = 0
     for i in range(-1,2):
         for j in range(-1,2):
             if i or j: # Don't count current cell 
-                if(0 <= row+i < len(board) and 0 <= col+j < len(board[row+i]) and board[row+i][col+j]):
-                    neighbors_count += 1
+                if 0 <= row+i < len(board) and 0 <= col+j < len(board[row+i]):
+                    neighbors_count += board[row+i][col+j]
     
     return neighbors_count
 
 
-def advance_generation(board):
+def advance_generation(board : list) ->list:
+    '''
+    Function advances the board 1 generation.
+    Input: the board in the current generation.
+    Output: the board in the new generation.
+    '''
     new_board = copy.deepcopy(board)
     for i in range(len(board)):
         for j in range(len(board[i])):
-            surround = sum_neighbors(i,j,board)
+            alive_neighbors = sum_neighbors(i,j,board)
             if board[i][j]:
-                if not (2 <= surround <= 3):
+                if not 2 <= alive_neighbors <= 3:
                     new_board[i][j] = DEAD
-            elif surround == 3:
+            elif alive_neighbors == 3:
                 new_board[i][j] = ALIVE
     return new_board
 
 
-def print_board(board):
+def print_board(board : list) ->None:
+    '''
+    Function clears all current output and prints a board of cells according to format.
+    Input: board to print.
+    Output: none
+    '''
     os.system('clear')
     print(MSG)
     for line in board:
@@ -76,20 +95,15 @@ def print_board(board):
         print('|')
 
 
-def main(args):
-    if len(args) != 2:
-        print("Please enter 2 arguments\n\t-file path to initial board state\n\t-number of generations")
-        exit()
+def main(args : argparse.Namespace) ->None:
     try:
-        board = init_board(args[0])
-        generations = int(args[1])
-    except ValueError:
-        print(f"2nd argument must be a number, '{args[1]}' was entered.")
-        exit()
+        board = init_board(args.file_path)
     except Exception as e:
         print(e)
         exit()
 
+    generations = int(args.generations)
+    
     for i in range(generations):
         print_board(board)
         time.sleep(100/1000) # sleep for 100 ms
@@ -97,4 +111,9 @@ def main(args):
 
 
 if __name__ =="__main__":
-    main(sys.argv[1:])
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file_path", help="the file path to the initial board state.")
+    parser.add_argument("generations", type=int, help="the number of generations to present.")
+    args = parser.parse_args()
+    print(type(args))
+    main(args)
