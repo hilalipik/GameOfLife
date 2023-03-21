fdb016d50659ea68b5774aa87b6cdee50136cf47
hilalipik <hilalipik@gmail.com>
2023-03-21 11:56:34 +0200

    Updated file_reader.py

--- file_reader.py
+++ file_reader.py
@@ -1,6 +1,11 @@
 import os
 
-def read_lines(file_path : str) -> str:
+def read_lines(file_path : str) -> list[str]:
+    '''
+    Reads lines from wanted file, may raise an error if file doesn't exist.
+    Input: file path.
+    Output: a list of the lines read fron file.
+    '''
     if not os.path.isfile(file_path):
         raise Exception(f"File {file_path} could not be found.")
     with open(file_path, mode="r") as file:
